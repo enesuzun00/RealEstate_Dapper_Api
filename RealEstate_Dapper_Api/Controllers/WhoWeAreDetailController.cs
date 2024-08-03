e70ebc7e7385d1ab37f4019708dd1ac965b420db
ENES <enesuzn00@gmail.com>
2024-08-03 21:16:26 +0300

    Add project files.

---
+++ RealEstate_Dapper_Api/Controllers/WhoWeAreDetailController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+using RealEstate_Dapper_Api.Dtos.WhoWeAreDetailDtos;
+using RealEstate_Dapper_Api.Repositories.CategoryRepository;
+using RealEstate_Dapper_Api.Repositories.WhoWeAreRepository;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WhoWeAreDetailController : ControllerBase
+    {
+        private readonly IWhoWeAreDetailRepository _whoWeAreDetailRepository;
+        public WhoWeAreDetailController(IWhoWeAreDetailRepository whoWeAreDetailRepository)
+        {
+            _whoWeAreDetailRepository = whoWeAreDetailRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> WhoWeAreDetailList()
+        {
+            var values = await _whoWeAreDetailRepository.GetAllWhoWeAreDetailAsync();
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateWhoWeAreDetail(CreateWhoWeAreDetailDto createWhoWeAreDetailDto)
+        {
+            _whoWeAreDetailRepository.CreateWhoWeAreDetail(createWhoWeAreDetailDto);
+            return Ok("Hakkımızda kısmı başarıyla eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteWhoWeAreDetail(int id)
+        {
+            _whoWeAreDetailRepository.DeleteWhoWeAreDetail(id);
+            return Ok("Hakkımızda kısmı başarıyla silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpadeteWhoWeAreDetail(UpdateWhoWeAreDetailDto updateWhoWeAreDetailDto)
+        {
+            _whoWeAreDetailRepository.UpdateWhoWeAreDetail(updateWhoWeAreDetailDto);
+            return Ok("Hakkımızda kısmı Güncellendi");
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetWhoWeAreDetail(int id)
+        {
+            var value = await _whoWeAreDetailRepository.GetWhoWeAreDetail(id);
+            return Ok(value);
+        }
+    }
+}
