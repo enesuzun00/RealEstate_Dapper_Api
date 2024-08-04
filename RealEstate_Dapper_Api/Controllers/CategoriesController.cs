3c91babd4ecb45ec6bab9f2362454cf897599bce
ENES <enesuzn00@gmail.com>
2024-08-04 23:34:11 +0300

    27.ders sonu

--- RealEstate_Dapper_Api/Controllers/CategoriesController.cs
+++ RealEstate_Dapper_Api/Controllers/CategoriesController.cs
@@ -27,7 +27,7 @@ namespace RealEstate_Dapper_Api.Controllers
             _categoryRepository.CreateCategory(createCategoryDto);
             return Ok("Kategori başarıyla eklendi");
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteCategory(int id)
         {
             _categoryRepository.DeleteCategory(id);
