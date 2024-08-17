b7e5c078aa5159a60d50963622d61ee3dff756c9
ENES <enesuzn00@gmail.com>
2024-08-17 23:18:40 +0300

    69.ders sonu- Giriş yapan kullanıcıya ait aktif pasif ilanlar

--- RealEstate_Dapper_UI/Areas/EstateAgent/Controllers/MyAdvertsController.cs
+++ RealEstate_Dapper_UI/Areas/EstateAgent/Controllers/MyAdvertsController.cs
@@ -16,11 +16,25 @@ namespace RealEstate_Dapper_UI.Areas.EstateAgent.Controllers
             _loginService = loginService;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> ActiveAdverts()
         {
             var id = _loginService.GetUserId;
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:44344/api/Products/ProductAdvertsListByEmployee?id="+ id);
+            var responseMessage = await client.GetAsync("https://localhost:44344/api/Products/ProductAdvertsListByEmployeeByTrue?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductAdvertListWithCategoryByEmployeeDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> PassiveAdverts()
+        {
+            var id = _loginService.GetUserId;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44344/api/Products/ProductAdvertListByEmployeeByFalse?id=" + id);
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
