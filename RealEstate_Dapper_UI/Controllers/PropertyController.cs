4f48e42bf96e1a1a9cf83c6cffba10a05c148638
ENES <enesuzn00@gmail.com>
2024-08-20 00:48:02 +0300

    76.ders sonu- Property single sayfasında ilan görselleri

--- RealEstate_Dapper_UI/Controllers/PropertyController.cs
+++ RealEstate_Dapper_UI/Controllers/PropertyController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.ProductDetailDtos;
 using RealEstate_Dapper_UI.Dtos.ProductDtos;
 
 namespace RealEstate_Dapper_UI.Controllers
@@ -27,8 +28,38 @@ namespace RealEstate_Dapper_UI.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult>PropertySingle(int id)
+        public async Task<IActionResult> PropertySingle(int id)
         {
+            id = 1;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44344/api/Products/GetProductByProductId?id=" + id);
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<ResultProductDto>(jsonData);
+
+            var responseMessage2 = await client.GetAsync("https://localhost:44344/api/ProductDetails/GetProductDetailByProductId?id=" + id);
+            var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+            var values2 = JsonConvert.DeserializeObject<GetProductDetailByIdDto>(jsonData2);
+
+            ViewBag.title1 = values.title.ToString();
+            ViewBag.price = values.price;
+            ViewBag.city = values.city;
+            ViewBag.district = values.district;
+            ViewBag.address = values.address;
+            ViewBag.type = values.type;
+
+            ViewBag.bathCount = values2.bathCount;
+            ViewBag.bedCount = values2.bedRoomCount;
+            ViewBag.size = values2.productSize;
+
+            DateTime date1= DateTime.Now;
+            DateTime date2= values.AdvertisementDate;
+
+            TimeSpan timeSpan = date1 - date2;
+            int month=timeSpan.Days;
+
+            ViewBag.datediff = month/30;
+
+
             return View();
         }
     }
