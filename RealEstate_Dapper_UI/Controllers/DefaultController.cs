6115e6c384b272d85c915278d823e9c04bd436a0
ENES <enesuzn00@gmail.com>
2024-08-20 22:37:56 +0300

    85.Video sonu- ev arama sayfası için partial oluşturma

--- RealEstate_Dapper_UI/Controllers/DefaultController.cs
+++ RealEstate_Dapper_UI/Controllers/DefaultController.cs
@@ -1,24 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.CategoryDtos;
 
 namespace RealEstate_Dapper_UI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpGet]
-        public PartialViewResult PartialSearch()
+        public async Task< PartialViewResult >PartialSearch()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44344/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return PartialView(values);
+            }
             return PartialView();
         }
 
         [HttpPost]
-        public IActionResult PartialSearch(string p)
+        public IActionResult PartialSearch(string p, string y)
         {
             TempData["word"]=p;
+            TempData["word1"]=y;
             return RedirectToAction("PropertyListWithSearch", "Property");
         }
     }
