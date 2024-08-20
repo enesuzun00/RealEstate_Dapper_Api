74fd6d82f6bb384497bf32d8f4f0e5661d3efe2f
ENES <enesuzn00@gmail.com>
2024-08-20 23:59:48 +0300

    86.video sonu- filtreleme işleminin tamalanması

--- RealEstate_Dapper_UI/Controllers/DefaultController.cs
+++ RealEstate_Dapper_UI/Controllers/DefaultController.cs
@@ -12,8 +12,16 @@ namespace RealEstate_Dapper_UI.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
-        public IActionResult Index()
+        public async Task< IActionResult> Index()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44344/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
 
@@ -32,10 +40,11 @@ namespace RealEstate_Dapper_UI.Controllers
         }
 
         [HttpPost]
-        public IActionResult PartialSearch(string p, string y)
+        public IActionResult PartialSearch(string searchKeyValue, int propertyCategoryId, string city)
         {
-            TempData["word"]=p;
-            TempData["word1"]=y;
+            TempData["searchKeyValue"] = searchKeyValue;
+            TempData["propertyCategoryId"] = propertyCategoryId;
+            TempData["city"] = city;
             return RedirectToAction("PropertyListWithSearch", "Property");
         }
     }
--- RealEstate_Dapper_UI/Controllers/PropertyController.cs
+++ RealEstate_Dapper_UI/Controllers/PropertyController.cs
@@ -29,11 +29,14 @@ namespace RealEstate_Dapper_UI.Controllers
 
         public async Task<IActionResult> PropertyListWithSearch(string searchKeyValue, int propertyCategoryId, string city)
         {
-            ViewBag.v= TempData["word"];
-            ViewBag.y= TempData["word1"];
-            searchKeyValue = "daire";
-            propertyCategoryId = 1;
-            city = "İzmir";
+            ViewBag.searchKeyValue = TempData["searchKeyValue"];
+            ViewBag.propertyCategoryId = TempData["propertyCategoryId"];
+            ViewBag.city = TempData["city"];
+
+            searchKeyValue = TempData["searchKeyValue"].ToString();
+            propertyCategoryId = int.Parse(TempData["propertyCategoryId"].ToString());
+            city = TempData["city"].ToString();
+
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync($"https://localhost:44344/api/Products/ResultProductWithSearchList?searchKeyValue={searchKeyValue}&propertyCategoryId={propertyCategoryId}&city={city}");
             if (responseMessage.IsSuccessStatusCode)
